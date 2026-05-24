b6826c4c5fd37e07fd65c66a61491256b233280f
aromarokiokaka <98908556+aromarokiokaka@users.noreply.github.com>
2026-05-25 00:25:42 +0200

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -31,8 +31,12 @@ def webhook():
 
     return "OK", 200
 
+import os
+
 if __name__ == "__main__":
-    app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 
